cbf92bf9b4195554c644b1a0a58748702bb07c10
Olga Chyzh <ochyzh1@gmail.com>
2021-07-19 13:35:15 -0400

    test

---
+++ R/data.R
@@ -0,0 +1,23 @@
+#' Dyadic data on bilateral alliance
+#'
+#' @format A data frame with 2256 rows and 5 variables:
+#' \describe{
+#'   \item{ccode1}{COW country code of the first country}
+#'   \item{ccode2}{COW country code of the second country}
+#'   \item{ij}{dyad id}
+#'   \item{defAlly}{equals 1 if the two countries were allied in that year}
+#'   \item{year}{year of observation}
+#' }
+#' @source ATOP data
+"defAlly"
+
+#' Highland ponies
+#'
+#' A dataset on groomin behavior of highland ponies
+#'
+#' @format A data matrix with 17 obs. of  18 variables:
+#' \describe{
+#'   \item{ï..}{name of the pony on the row}
+#' }
+#' @source Shahryar Minhas
+"highlandPonies"
